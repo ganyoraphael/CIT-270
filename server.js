93ef168577d485072bb97b9e143a8c9b885ea08a
ganyoraphael <104370980+ganyoraphael@users.noreply.github.com>
2023-01-18 13:47:53 -0700

    HelloWorld Test Build Up

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require ("express");
+
 const app = express();
 
 const port = 3000;
 
+const bodyParser = require ('body-parser');
+
+app.use(bodyParser.json()); //This looks for incoming data
+
 app.get("/", (req, res) => {
-    res.send("hello Raphael");
+    res.send("Hello Raphael!");
+});
+
+app.post('/login', (req, res) => {
+    const loginUser = req.body.userName;
+    console.log('Login username: '+loginUser);
+    res.send('Hello '+loginUser);
 });
 
 app.listen(port, () => {
